6e9fa1690d564bc7a66d4962b9ab006b3df1bf94
brightigiemokha <brightigiemokha@yahoo.com>
2023-03-30 20:20:16 +0000

    added if statement

--- run.py
+++ run.py
@@ -21,6 +21,18 @@ def get_sales_data():
     print("Example: 05,35,45,25\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    # to remove commas from the strings
+    sales_data = data_str.split(",")
+    print(sales_data)
+
+def validate_data(values):
+    # to raise valueError and conver all strings into integers if there aren't 5 values
+    try:
+        if lent(values) != 4:
+            raise ValueError(
+                f" Please check your input, only 4 value required, you have ended {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invald input: {e}, please try again.\n")
 
 get_sales_data()
